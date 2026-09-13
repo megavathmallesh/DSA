0f230048ea9dd4df9cd413642fc35554643ab73e
Megavath Mallesh <168923451+megavathmallesh@users.noreply.github.com>
2026-09-13 16:35:44 +0530

    Time: 33 ms (80.04%), Space: 82.6 MB (95.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
-       unordered_set<int>ans;
+    //    unordered_set<int>ans;
 
-       for(int i=0; i<n; i++) {
-        if(ans.find(nums[i]) != ans.end()) {
-            return true;
+    //    for(int i=0; i<n; i++) {
+    //     if(ans.find(nums[i]) != ans.end()) {
+    //         return true;
+    //     }
+    //     ans.insert(nums[i]);
+    //    }
+    //    return false;
+
+    sort(nums.begin(), nums.end());
+
+        for(int i = 1; i<n; i++) {
+            
+            if(nums[i] == nums[i-1]) {
+                return true;
+            }
         }
-        ans.insert(nums[i]);
-       }
-       return false;
+       return false; 
+
+
+
     }
 };
